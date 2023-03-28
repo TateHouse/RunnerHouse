eb79907127f4f23759bb8cd9efa73eb78deb4025
Tate House <tatehouse100@gmail.com>
2023-03-28 11:18:39 -0400

    Implement dynamically choosing a pair of shorts
    The BOA 1" Elite Splits Shorts image on the gear page is now dynamically
    chosen when the page is loaded. The alt descriptions and URLs for each
    pair are also updated to match the chosen pair.

---
+++ javascript/set-shorts-image.js
@@ -0,0 +1,62 @@
+function selectShortsImage() {
+    const images = ["images/shorts/boausa-elite-split-buffalo-plaid.png",
+                    "images/shorts/boausa-elite-split-ducky.png",
+                    "images/shorts/boausa-elite-split-grunge.png",
+                    "images/shorts/boausa-elite-split-thin-blue-line-flag.png",
+                    "images/shorts/boausa-elite-split-ugly-sweater.png",
+                    "images/shorts/boausa-elite-split-usa.png"];
+
+    const altDescriptions = ["A pair of 1 inch shorts from BOA with a buffalo plaid design",
+                             "A pair of 1 inch shorts from BOA with a rubber ducky design",
+                             "A pair of 1 inch shorts from BOA with a grunge design",
+                             "A pair of 1 inch shorts from BOA with a thin blue line flag design",
+                             "A pair of 1 inch shorts from BOA with an ugly christmas sweater design",
+                             "A pair of 1 inch shorts from BOA with a USA flag design"];
+
+    function getImageElement() {
+        return document.getElementById("shorts-img");
+    }
+
+    function generateRandomIndex() {
+        return Math.floor(Math.random() * images.length);
+    }
+
+    function setImage(imageElement, imageIndex) {
+        imageElement.src = images[imageIndex];
+    }
+
+    function setAltDescription(imageElement, altIndex) {
+        imageElement.alt = altDescriptions[altIndex];
+    }
+
+    const imageElement = getImageElement();
+    const index = generateRandomIndex();
+    setImage(imageElement, index);
+    setAltDescription(imageElement, index);
+
+    return index;
+}
+
+
+function selectShortsURL(index) {
+    const urls = ["https://www.boausa.com/collections/1-elite-shorts/products/mens-stretch-1-elite-printed-split-run-short-buffalo-plaid",
+                  "https://www.boausa.com/collections/1-elite-shorts/products/mens-stretch-1-elite-printed-split-run-short-rubber-duckie",
+                  "https://www.boausa.com/collections/1-elite-shorts/products/mens-1-elite-split-shorts-grunge",
+                  "https://www.boausa.com/collections/1-elite-shorts/products/mens-1-inch-inseam-elite-split-running-shorts-thin-blue-line",
+                  "https://www.boausa.com/collections/1-elite-shorts/products/mens-stretch-1-elite-printed-split-run-short-ugly-sweater",
+                  "https://www.boausa.com/collections/1-elite-shorts/products/1000swp-sab"];
+
+    function getLinkElement() {
+        return document.getElementById("shorts-link");
+    }
+
+    function setURL(linkElement, index) {
+        linkElement.href = urls[index];
+    }
+
+    const linkElement = getLinkElement();
+    setURL(linkElement, index);
+}
+
+const index = selectShortsImage();
+selectShortsURL(index);
